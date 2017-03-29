c6458cfc5c957b5c06a7af3b8ba535310a8da299
Justin <justin6689@gmail.com>
2017-03-29 21:50:54 +0800

    edit home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,88 +8,61 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        <link href="css/app.css" rel="stylesheet">
+        <script src="js/app.js"></script>
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+        <style type="text/css">
+            body {
+                padding-top: 120px;
+                background: #FFF;
             }
-
-            .full-height {
-                height: 100vh;
+            header {
+                height: 50px;
+                background: #434343;
+                line-height: 50px;
+                padding: 0 35px;
             }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
+            header>div {
+                max-width: 1024px;
+                margin: 0 auto;
             }
+            header span {
 
-            .position-ref {
-                position: relative;
+                font-size: 13px;
+                color: #FFF;
             }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
+            nav {
+                height: 70px;
+                background: #FFF;
             }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .header-fixed {
+                width: 100%;
+                position: fixed;
+                top: 0;
+                z-index: 9999;
             }
         </style>
+
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+    <div class="header-fixed">
+    <header>
+        <div>
+            <span>{{ __('static.header_tel') }}</span>
+            <span style="text-align: right">繁 | 簡 | 日文 | English</span>
         </div>
+    </header>
+    <nav>
+        <img src="images/logo.png">
+    </nav>
+</div>
+  <div class="container-fluid" style="background:#F00">
+    <div class="btn-group" role="group" aria-label="...">
+      <button type="button" class="btn btn-default">Left</button>
+      <button type="button" class="btn btn-default">Middle</button>
+      <button type="button" class="btn btn-default">Right</button>
+    </div></div>
+    a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>a<p>
     </body>
 </html>
