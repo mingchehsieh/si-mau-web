10ca2b2da56a67b14a317431f038e58c6ff03231
Mingche <justin6689@gmail.com>
2017-07-07 02:27:04 +0800

    modify scrolltop 50

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,6 @@
 ;(function($){
 
-	var num = 150; //number of pixels before modifying styles
+	var num = 50; //number of pixels before modifying styles
 
 	$(window).bind('scroll', function () {
 	    if ($(window).scrollTop() > num) {
